1dea8b81999e653ba941e42fcd141b317033ebdc
Fantasma2009 <95443797+Fantasma2009@users.noreply.github.com>
2022-06-08 17:27:22 -0300

    Add files via upload

---
+++ papper.js
@@ -0,0 +1,15 @@
+class papper{
+    constructor(x,y)
+    {var options={
+        isStathic:false,
+        restituction:0.3,
+        friction:0,
+        density:1.2
+    }
+        display()
+    
+        this.image=loadImage("papper.png");
+        ellipse(0,0,this.r, this.r);
+
+    }
+}
